0c361270c758cc6d6bdb4d7256eef04f59550934
isa703 <116504377+isa703@users.noreply.github.com>
2026-07-25 17:33:11 +0300

    Update metadata and improve code formatting

--- __init__.py
+++ __init__.py
@@ -1,11 +1,10 @@
-
 bl_info = {
     "name": "Gothic Architect",
-    "author": "OpenAI",
-    "version": (1, 0, 0),
-    "blender": (4, 0, 0),
-    "location": "View3D > Sidebar > Gothic Architect",
-    "description": "Procedural Gothic architecture generator",
+    "author": "Isa703 & OpenAI",
+    "version": (0, 2, 0),
+    "blender": (5, 1, 0),
+    "location": "View3D > Sidebar > Gothic",
+    "description": "Procedural Gothic Architecture Generator",
     "category": "Add Mesh",
 }
 
@@ -22,14 +21,18 @@ classes = (
     ui.GA_PT_main_panel,
 )
 
+
 def register():
     for cls in classes:
         bpy.utils.register_class(cls)
 
-    bpy.types.Scene.ga_props = bpy.props.PointerProperty(type=properties.GA_ColumnProperties)
+    bpy.types.Scene.ga_props = bpy.props.PointerProperty(
+        type=properties.GA_ColumnProperties
+    )
 
 
 def unregister():
+
     del bpy.types.Scene.ga_props
 
     for cls in reversed(classes):

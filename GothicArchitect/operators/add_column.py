bc6cf8af1c51b560ec46b6bd745da9e379c3b709
isa703 <116504377+isa703@users.noreply.github.com>
2026-07-25 18:45:56 +0300

    Update add_column.py

--- GothicArchitect/operators/add_column.py
+++ GothicArchitect/operators/add_column.py
@@ -1,1 +1,30 @@
+import bpy
 
+from ..geometry.column import build_column
+
+
+class GA_OT_AddColumn(bpy.types.Operator):
+    """Create a procedural Gothic column"""
+
+    bl_idname = "ga.add_column"
+    bl_label = "Create Gothic Column"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):
+
+        props = context.scene.ga_props
+
+        build_column(
+            context=context,
+            height=props.height,
+            radius=props.radius,
+            base_radius=props.base_radius,
+            capital_height=props.capital_height,
+            capital_radius=props.capital_radius,
+            shafts=props.shafts,
+            seed=props.seed,
+        )
+
+        self.report({'INFO'}, "Gothic column created.")
+
+        return {'FINISHED'}
